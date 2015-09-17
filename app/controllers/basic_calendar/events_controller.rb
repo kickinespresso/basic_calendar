7c981a49194c3bb0877538d3ab922f86e1cc6351
Chris Desch <cdesch@gmail.com>
2015-09-17 13:11:31 -0400

    Removing views
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- app/controllers/basic_calendar/events_controller.rb
+++ app/controllers/basic_calendar/events_controller.rb
@@ -2,7 +2,7 @@ require_dependency "basic_calendar/application_controller"
 
 module BasicCalendar
   class EventsController < ApplicationController
-    before_action :set_event, only: [:show, :edit, :update, :destroy]
+    before_action :set_event, only: [:show]
 
     # GET /events
     def index
@@ -13,40 +13,6 @@ module BasicCalendar
     def show
     end
 
-    # GET /events/new
-    def new
-      @event = Event.new
-    end
-
-    # GET /events/1/edit
-    def edit
-    end
-
-    # POST /events
-    def create
-      @event = Event.new(event_params)
-
-      if @event.save
-        redirect_to @event, notice: 'Event was successfully created.'
-      else
-        render :new
-      end
-    end
-
-    # PATCH/PUT /events/1
-    def update
-      if @event.update(event_params)
-        redirect_to @event, notice: 'Event was successfully updated.'
-      else
-        render :edit
-      end
-    end
-
-    # DELETE /events/1
-    def destroy
-      @event.destroy
-      redirect_to events_url, notice: 'Event was successfully destroyed.'
-    end
 
     private
       # Use callbacks to share common setup or constraints between actions.
